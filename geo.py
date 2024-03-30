cf808a8b940c4d25353d12eb8b560ea4e84ef7af
IDRERD <idrerd999@gmail.com>
2024-03-30 13:42:56 +0400

    Alice and The Maps get_geo_info

--- geo.py
+++ geo.py
@@ -39,3 +39,27 @@ def get_distance(p1, p2):
     c = 2 * math.atan2(a ** 0.5, (1 - a) ** 0.5)
     distance = radius * c
     return distance
+
+
+def get_geo_info(city_name, type_info):
+    if type_info == "coordinates":
+        try:
+            url = f"https://geocode-maps.yandex.ru/1.x/"
+            params = {"apikey": "40d1649f-0493-4b70-98ba-98533de7710b", "geocode": city_name, "format": "json"}
+            response = requests.get(url, params=params)
+            js = response.json()
+            coordinates_str = js["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"]
+            lng, lat = map(float, coordinates_str.split())
+            return lng, lat
+        except Exception as e:
+            return e
+    elif type_info == "country":
+        try:
+            url = f"https://geocode-maps.yandex.ru/1.x/"
+            params = {"apikey": "40d1649f-0493-4b70-98ba-98533de7710b", "geocode": city_name, "format": "json"}
+            response = requests.get(url, params=params)
+            js = response.json()
+            return js["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["metaDataProperty"][
+                "GeocoderMetaData"]["AddressDetails"]["Country"]["CountryName"]
+        except Exception as e:
+            return e
